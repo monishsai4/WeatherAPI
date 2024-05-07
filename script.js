cc41127b197747b838f03e0c63592eca0c1951ea
Monish Sai <monish030404@gmail.com>
2024-05-07 16:00:40 +0530

    Second commit

--- script.js
+++ script.js
@@ -8,7 +8,8 @@ weatherForm.addEventListener("submit", async (event) => {
   if (city) {
     try {
       const weatherData = await getWeatherData(city);
-      displayWeatherInfo(weatherData);
+      displayWeatherData(weatherData);
+      console.log(weatherData);
     } catch (error) {
       console.log.error(error);
       displayError(error);
@@ -17,19 +18,41 @@ weatherForm.addEventListener("submit", async (event) => {
     displayError("Please enter a city");
   }
 });
-
+function goToFIll() {}
 async function getWeatherData(city) {
   const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apikey}`;
   const response = await fetch(apiUrl);
   return await response.json();
 }
 
-function displayWeatherInfo(data) {
+function displayWeatherData(data) {
   const {
     name: city,
-    main: { temp, humidity },
-    weather: [{ description, id }],
+    wind: { speed },
+    visibility: value,
+    main: { temp, humidity, pressure },
+    sys: { sunrise, sunset },
+    weather: [{ description }],
   } = data;
+  var divElement = document.getElementById("Locate");
+  divElement.textContent = city;
+  var divLocate = document.getElementById("GetTemp");
+  const Celsius = temp - 273.15;
+  divLocate.innerHTML = Math.trunc(Celsius);
+  var divDescription = document.getElementById("getType");
+  divDescription.innerHTML = description;
+  var divhumidity = document.getElementById("Humiditt");
+  divhumidity.innerHTML = humidity;
+  var divpressure = document.getElementById("Pressuree");
+  divpressure.innerHTML = pressure;
+  var divsunrise = document.getElementById("sunrise");
+  divsunrise.innerHTML = sunrise;
+  var divsunset = document.getElementById("sunset");
+  divsunset.innerHTML = sunset;
+  var divspeed = document.getElementById("speed");
+  divspeed.innerHTML = speed;
+  var divvalue = document.getElementById("value");
+  divvalue.innerHTML = value;
 }
 
 function displayWeatherInfo(weatherId) {}
